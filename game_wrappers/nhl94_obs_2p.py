974d2cf969b7129643e4751a548e0951024b6a81
MatPoliquin <mathieu.poliquin@gmail.com>
2023-12-05 14:48:23 +0800

    NHL94 env: move discretizer wrapper to it's own file

--- game_wrappers/nhl94_obs_2p.py
+++ game_wrappers/nhl94_obs_2p.py
@@ -22,29 +22,6 @@ from game_wrappers.nhl94_ai import NHL94AISystem
 
 from game_wrappers.nhl94_gamestate import NHL94GameState
 
-#https://github.com/openai/retro-baselines/blob/master/agents/sonic_util.py
-class NHL94Discretizer(gym.ActionWrapper):
-    """
-    Wrap a gym-retro environment and make it use discrete
-    actions for the Sonic game.
-    """
-    def __init__(self, env):
-        super(NHL94Discretizer, self).__init__(env)
-        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
-        actions = [['LEFT'], ['RIGHT'], ['UP'], ['DOWN'], ['A'], ['DOWN', 'LEFT'], ['DOWN', 'RIGHT'], ['UP', 'LEFT'], ['UP', 'RIGHT']]
-        self._actions = []
-        for action in actions:
-            arr = np.array([False] * 12)
-            for button in action:
-                arr[buttons.index(button)] = True
-            self._actions.append(arr)
-        self.action_space = gym.spaces.Discrete(len(self._actions))
-
-    def action(self, a): # pylint: disable=W0221
-        return self._actions[a].copy()
-
-
-# WARNING: NON FUNCTIONAL CODE - WIP
 class NHL94Observation2PEnv(gym.Wrapper):
     def __init__(self, env, args, num_players, rf_name):
         gym.Wrapper.__init__(self, env)
