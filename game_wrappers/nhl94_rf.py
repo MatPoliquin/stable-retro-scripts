a8a72b7b56599eeb7c4b579787cd45b32ac863ec
MatPoliquin <mathieu.poliquin@gmail.com>
2023-12-19 19:24:02 +0800

    NHL94 env: added ScoreGoal02 reward function

--- game_wrappers/nhl94_rf.py
+++ game_wrappers/nhl94_rf.py
@@ -6,9 +6,9 @@ import random
 from game_wrappers.nhl94_gamestate import NHL94GameState
 
 
-
-
-
+# =====================================================================
+# Common functions
+# =====================================================================
 def RandomPos():
     x = (random.random() - 0.5) * 235
     y = (random.random() - 0.5) * 460
@@ -30,114 +30,23 @@ def RandomPosDefenseZone():
     #print(x,y)
     return x, y
 
+# =====================================================================
+# General
+# =====================================================================
 def rf_general(state):
     
-    player_haspuck = False
-    goalie_haspuck = False
-
-    isGoodShot = True
-    rew = 0
-
-
-    # distToAttackZone = 120 - state.p1_y 
-
-    # if player_haspuck == False:
-    #     if p1_y > 120 and p1_shots > self.last_p1_shots:
-    #         self.lastshot_time = self.time
-    #         rew = 1.0
-    #     elif self.last_dist != -1:
-    #         if distToPuck < self.last_dist:
-    #             rew = 0.3
-    #         else:
-    #             rew = -1
-    # else:
-    #     if p1_y < 120 and self.last_dist_az != -1:
-    #         if distToAttackZone < self.last_dist_az:
-    #             rew = 0.3
-    #         else:
-    #             rew = -1
-
-
-    # if p1_bodychecks > self.last_p1_bodychecks:
-    #     rew = 0.5
-
-    # #     if p1_y > 120: rew = 0.0
-
-    # #if p1_faceoffwon > self.last_p1_faceoffwon:
-    # #     rew = 0.5
-            
+    if state.p1_score > state.last_p1_score: 
+        rew = 1.0
     
-    # if self.lastshot_time != -1:
-    #     if (self.time - self.lastshot_time > 60):
-    #         self.lastshot_time = -1
-    #     else:
-    #         rew = 1.0
-    
-    # if goalie_haspuck:
-    #     rew = -1
-
-    # if p1_passing > self.last_p1_passing:
-    #     rew = 0.5
-    
-
-    # #if p1_attackzone > self.last_p1_attackzone and p1_shots > self.last_p1_shots:
-    # #    rew = 0.2
-
-    # if p1_score > self.last_p1_score:
-    #     rew = 1.0
-
-    # #if p1_shots > self.last_p1_shots:
-    # #    rew = 0.1
-
-
-    # #if p2_attackzone > self.last_p2_attackzone:
-    # #   rew = -0.2
-        
-    # if p2_score > self.last_p2_score:
-    #     rew = -1.0
-
-    # if p2_shots > self.last_p2_shots:
-    #     rew = -1.0
-
-
-    # self.last_p1_score = p1_score
-    # self.last_p1_shots = p1_shots
-    # self.last_p1_bodychecks = p1_bodychecks
-    # self.last_p2_attackzone = p2_attackzone
-    # self.last_p1_attackzone = p1_attackzone
-    # self.last_p1_faceoffwon = p1_faceoffwon
-    # self.last_p2_shots = p2_shots
-    # self.last_p2_score = p2_score
-    # self.last_time = time
-    # self.last_p1_passing = p1_passing
-    # self.last_dist = distToPuck
-    # self.last_dist_az = distToAttackZone
-
-    # #last_p2_pos
-
-
-    # #if abs(p1_x - p2_x) > 50 or abs(p1_y - p2_y) > 50:
-    # #    rew = -1.0
-    # #elif abs(p1_x - p2_x) > 30 or abs(p1_y - p2_y) > 30:
-    # #    rew = 0.5
-    # #else:
-    # #    rew = 1.0
-
-
-    # # Don't give rewards when clock is not running
-    # if puck_x == 0 and puck_y == 0:
-    #     rew = 0
-
-    # #if abs(g1_x - puck_x) <= 6 and abs(g1_y - puck_y) <= 6:
-    # #    rew = -1
+    if state.p2_score > state.last_p2_score: 
+        rew = -1.0
 
     return rew
 
+# =====================================================================
+# ScoreGoal
+# =====================================================================
 def init_scoregoal(env):
-    #x, y = self.RandomPos()
-    #self.env.set_value("rpuck_x", x)
-    #self.env.set_value("rpuck_y", y)
-
     x, y = RandomPosAttackZone()
     env.set_value("p2_x", x)
     env.set_value("p2_y", y)
@@ -183,13 +92,66 @@ def rf_scoregoal(state):
                     rew = 1.0
                 else:
                     rew = 0.5
-        
-    #TODO reward good shots
-    #if state.p1_shots > state.last_p1_shots: 
-    #    rew = 1.0
 
     return rew
 
+# =====================================================================
+# ScoreGoal02
+# =====================================================================
+def init_scoregoal02(env):
+    x, y = RandomPosAttackZone()
+    env.set_value("p2_x", x)
+    env.set_value("p2_y", y)
+
+    x, y = RandomPosAttackZone()
+    env.set_value("p1_x", x)
+    env.set_value("p1_y", y)
+
+def isdone_scoregoal02(state):
+
+    if state.p1_score > state.last_p1_score:
+        return True
+    
+    #if state.p1_shots > state.last_p1_shots:
+    #    init_scoregoal02(env)
+
+    #if state.p2_haspuck or state.g2_haspuck:
+    #    return True
+    
+    #if state.puck_y < 100:
+    #    return True
+    
+    if state.time < 250:
+        return True
+
+    return False
+    
+def rf_scoregoal02(state):
+    
+    rew = 0.0
+
+    if state.p2_haspuck or state.g2_haspuck:
+        rew = -1.0
+    
+    if state.puck_y < 100:
+        rew = -1.0
+    
+    if state.p1_score > state.last_p1_score: 
+        rew = 1.0
+
+    # reward scoring opportunities
+    if state.player_haspuck and state.p1_y < 230 and state.p1_y > 210:
+        if state.p1_vel_x >= 30 or state.p1_vel_x <= -30:
+            rew = 0.2
+            if state.puck_x > -23 and state.puck_x < 23:
+                if state.p1_shots > state.last_p1_shots: 
+                    rew = 1.0
+
+    return rew
+
+# =====================================================================
+# KeepPuck
+# =====================================================================
 def init_keeppuck(env):
     #x, y = self.RandomPos()
     #self.env.set_value("rpuck_x", x)
@@ -215,6 +177,9 @@ def rf_keeppuck(state):
 
     return rew
 
+# =====================================================================
+# GetPuck
+# =====================================================================
 def init_getpuck(env):
     #x, y = self.RandomPos()
     #self.env.set_value("rpuck_x", x)
@@ -267,7 +232,9 @@ def rf_getpuck(state):
 
     return rew
 
-
+# =====================================================================
+# DefenseZone
+# =====================================================================
 def init_defensezone(env):
     #x, y = self.RandomPos()
     #self.env.set_value("rpuck_x", x)
@@ -332,12 +299,16 @@ def rf_defensezone(state):
 
     return rew
 
-
+# =====================================================================
+# Register Functions
+# =====================================================================
 def register_functions(name):
     if name == "GetPuck":
         return init_getpuck, rf_getpuck, isdone_getpuck
     elif name == "ScoreGoal":
         return init_scoregoal, rf_scoregoal, isdone_scoregoal
+    elif name == "ScoreGoal02":
+        return init_scoregoal02, rf_scoregoal02, isdone_scoregoal02
     elif name == "KeepPuck":
         return init_keeppuck, rf_keeppuck, isdone_keeppuck
     elif name == "DefenseZone":
