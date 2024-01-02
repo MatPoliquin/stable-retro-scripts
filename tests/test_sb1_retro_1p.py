e5bfc9439500b8df81e61e45f94b89f04274dfd1
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2024-01-02 11:59:52 +0800

    Updated PPO params in test_sb1_retro_1p.py

--- tests/test_sb1_retro_1p.py
+++ tests/test_sb1_retro_1p.py
@@ -2,24 +2,27 @@ import retro
 import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.atari_wrappers import WarpFrame, ClipRewardEnv
 import gymnasium as gym
 
 GAME_ENV = 'Pong-Atari2600'
 STATE_1P = 'Start'
 POLICY = 'CnnPolicy'
-TIMESTEPS = 1000
+TIMESTEPS = 10000000
 
 def main():
     # Create env and apply wrappers
     env = retro.make(game=GAME_ENV, state=STATE_1P, render_mode="rgb_array")
     env = WarpFrame(env)
-    env = ClipRewardEnv(env) 
+    env = ClipRewardEnv(env)
+    env = Monitor(env)
     env = DummyVecEnv([lambda: env])
+    
     env = VecFrameStack(env, n_stack=4)    
 
     # Train model
-    model = PPO(policy=POLICY, env=env, verbose=True)
+    model = PPO(policy=POLICY, env=env, verbose=True, ent_coef=0.01, batch_size=256)
     model.learn(total_timesteps=TIMESTEPS)
 
     # Test the trained model
