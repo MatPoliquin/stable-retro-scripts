bac5d6c3db0afe934ecf0206771e6572469df83c
MatPoliquin <mathieu.poliquin@gmail.com>
2024-06-28 17:18:51 +0800

    Added support for Windows

--- ef_lib/test.cpp
+++ ef_lib/test.cpp
@@ -1,27 +1,58 @@
 // test of game ai dynamic lib
 #include <iostream>
 #include <assert.h>
-#include <dlfcn.h>
 #include <filesystem>
 #include "GameAI.h"
+
+#ifdef _WIN32
+#include <windows.h> 
+#else
+#include <dlfcn.h>
+#endif
 
 
 void test_loadlibrary()
 {
+    GameAI * ga = nullptr;
+
+#ifdef _WIN32
+    HINSTANCE hinstLib; 
+    BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
+
+    hinstLib = LoadLibrary(TEXT("libgame_ai.dll"));
+
+    if (hinstLib != NULL) 
+    { 
+        creategameai_t func  = (creategameai_t) GetProcAddress(hinstLib, "CreateGameAI"); 
+ 
+        // If the function address is valid, call the function.
+        if (NULL != func) 
+        {
+            fRunTimeLinkSuccess = TRUE;
+            ga = func("NHL941on1-Genesis");
+            assert(ga);
+        }
+        // Free the DLL module.
+ 
+        fFreeResult = FreeLibrary(hinstLib); 
+    } 
+#else
     void *myso = dlopen("./libgame_ai.so", RTLD_NOW);
     assert(myso);
     creategameai_t func = reinterpret_cast<creategameai_t>(dlsym(myso, "CreateGameAI"));
     assert(func);
 
-    GameAI * ga = func("NHL941on1-Genesis");
+    ga = func("NHL941on1-Genesis");
     assert(ga);
+#endif
+
+    if(ga)
+        std::cout << "TEST PASSED!" << std::endl;
 }
 
 
 int main()
 {
-    std::cout << "HELLO!!" << std::endl;
-
     test_loadlibrary();
 
     return 0;
