3ca01f1ae93b74d52dbf9e9354ba74e651590fab
MatPoliquin <mathieu.poliquin@gmail.com>
2024-07-26 16:58:49 +0800

    code clean-up of export_model

--- export_model.py
+++ export_model.py
@@ -1,21 +1,21 @@
 # TEST adapted from stable-baselines3 docs
 # https://stable-baselines3.readthedocs.io/en/master/guide/export.html
 
+import sys
+import argparse
 import torch as th
 import torchvision
 from typing import Tuple
-
+import onnx
+import onnxruntime as ort
+import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.policies import BasePolicy
 
 
-#MODEL_PATH = "./models/ScoreGoal.zip"
-#jit_path = "ScoreGoal.pt"
-
-MODEL_PATH = "./models/DefenseZone.zip"
-jit_path = "DefenseZone.pt"
-
-
+#==========================================
+# OnnxableSB3Policy
+#==========================================
 class OnnxableSB3Policy(th.nn.Module):
     def __init__(self, policy: BasePolicy):
         super().__init__()
@@ -29,67 +29,115 @@ class OnnxableSB3Policy(th.nn.Module):
         # policy() returns `actions, values, log_prob` for PPO
         return self.policy(observation, deterministic=True)
 
+#==========================================
+# parse_cmdline
+#==========================================
+def parse_cmdline(argv):
+    parser = argparse.ArgumentParser()
 
-# Example: model = PPO("MlpPolicy", "Pendulum-v1")
-model = PPO.load(MODEL_PATH, device="cpu")
+    parser.add_argument('--src', type=str, default='./models/ScoreGoal.zip')
+    parser.add_argument('--dest', type=str, default='./models/ScoreGoal')
 
-onnx_policy = OnnxableSB3Policy(model.policy)
+    print(argv)
+    args = parser.parse_args(argv)
 
-observation_size = model.observation_space.shape
-print(observation_size)
-dummy_input = th.randn(1, *observation_size)
-th.onnx.export(
-    onnx_policy,
-    dummy_input,
-    "my_ppo_model.onnx",
-    opset_version=17,
-    input_names=["input"],
-)
+    return args
 
-# Pytorch JIT
-# See "ONNX export" for imports and OnnxablePolicy
+#==========================================
+# export_onnx
+#==========================================
+def export_onnx(args):
+    model = PPO.load(args.src, device="cpu")
+
+    onnx_policy = OnnxableSB3Policy(model.policy)
+
+    observation_size = model.observation_space.shape
+    print(observation_size)
+    dummy_input = th.randn(1, *observation_size)
+    dest_onnx_model = args.dest + ".onnx" # "my_ppo_model.onnx"
+    
+    th.onnx.export(
+        onnx_policy,
+        dummy_input,
+        dest_onnx_model,
+        opset_version=17,
+        input_names=["input"])
+    
+    return model, onnx_policy, dummy_input, dest_onnx_model
+
+#==========================================
+# export_pytorch
+#==========================================
+def export_pytorch(args, model, onnx_policy, dummy_input):
+    # Pytorch JIT
+    # See "ONNX export" for imports and OnnxablePolicy
+
+    # Trace and optimize the module
+    traced_module = th.jit.trace(onnx_policy.eval(), dummy_input)
+    frozen_module = th.jit.freeze(traced_module)
+    frozen_module = th.jit.optimize_for_inference(frozen_module)
+    #th.jit.save(frozen_module, jit_path)
+    dest_pytorch_model = args.dest + ".pt"
+    th.jit.save(traced_module, dest_pytorch_model)
+
+    #test resnet
+    #resnet_model = torchvision.models.resnet18()
+    #example = th.rand(1, 3, 224, 224)
+    #traced_script_module = th.jit.trace(resnet_model, example)
+    #traced_script_module.save("traced_resnet_model.pt")
+
+    return dest_pytorch_model
+
+#==========================================
+# test_models
+#==========================================
+def test_models(args, model, dest_onnx_model, dest_pytorch_model):
+    ## torch test
+    print(th.__version__)
+    observation_size = model.observation_space.shape
+    dummy_input = th.zeros(1, *observation_size)
+    loaded_module = th.jit.load(dest_pytorch_model)
+    action_jit = loaded_module(dummy_input)
+    print("Pytorch test")
+    print(observation_size)
+    print(dummy_input)
+    print(action_jit)
+    print("=========================")
+
+    # onnx test
+    onnx_model = onnx.load(dest_onnx_model)
+    onnx.checker.check_model(onnx_model)
+    observation_size = model.observation_space.shape
+    observation = np.zeros((1, *observation_size)).astype(np.float32)
+    ort_sess = ort.InferenceSession(dest_onnx_model)
+    actions, values, log_prob = ort_sess.run(None, {"input": observation})
+
+    print(actions, values, log_prob)
+
+    # Check that the predictions are the same
+    with th.no_grad():
+        print(model.policy(th.as_tensor(observation), deterministic=True))
 
 
-# Trace and optimize the module
-traced_module = th.jit.trace(onnx_policy.eval(), dummy_input)
-frozen_module = th.jit.freeze(traced_module)
-frozen_module = th.jit.optimize_for_inference(frozen_module)
-th.jit.save(frozen_module, jit_path)
+#==========================================
+# main
+#==========================================
+def main(argv):
+    
+    args = parse_cmdline(argv[1:])
 
-#test resnet
-resnet_model = torchvision.models.resnet18()
-example = th.rand(1, 3, 224, 224)
-traced_script_module = th.jit.trace(resnet_model, example)
-traced_script_module.save("traced_resnet_model.pt")
+    print("EXPORTING MODELS...")
+    model, onnx_policy, dummy_input, dest_onnx_model = export_onnx(args)
 
-##### Load and test with torch
-print(th.__version__)
+    dest_pytorch_model = export_pytorch(args, model, onnx_policy, dummy_input)
 
-dummy_input = th.zeros(1, *observation_size)
-loaded_module = th.jit.load(jit_path)
-action_jit = loaded_module(dummy_input)
-print("Pytorch test")
-print(observation_size)
-print(dummy_input)
-print(action_jit)
-print("=========================")
+    print("DONE EXPORTING MODELS")
+    print(dest_onnx_model)
+    print(dest_pytorch_model)
 
-##### Load and test with onnx
+    print("TESTING MODELS...")
+    test_models(args, model, dest_onnx_model, dest_pytorch_model)
 
-import onnx
-import onnxruntime as ort
-import numpy as np
 
-onnx_path = "my_ppo_model.onnx"
-onnx_model = onnx.load(onnx_path)
-onnx.checker.check_model(onnx_model)
-
-observation = np.zeros((1, *observation_size)).astype(np.float32)
-ort_sess = ort.InferenceSession(onnx_path)
-actions, values, log_prob = ort_sess.run(None, {"input": observation})
-
-print(actions, values, log_prob)
-
-# Check that the predictions are the same
-with th.no_grad():
-    print(model.policy(th.as_tensor(observation), deterministic=True))
+if __name__ == '__main__':
+    main(sys.argv)
