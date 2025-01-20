3fbe5e6146cfbc83bcdd784b3ecc4ff8f964ee32
MatPoliquin <mathieu.poliquin@gmail.com>
2025-01-20 11:35:16 +0800

    Fix pylint errors

--- common.py
+++ common.py
@@ -2,34 +2,37 @@
 Common utils
 """
 
-import warnings
-warnings.filterwarnings("ignore")
-import os, datetime
-import argparse
+
+import os
+import datetime
 
 from stable_baselines3.common.logger import configure
 
-logger = None
+LOGGER = None
 
 def create_output_dir(args):
+    """create_output_dir"""
     output_dir = args.env + datetime.datetime.now().strftime('-%Y-%m-%d_%H-%M-%S')
     output_fullpath = os.path.join(os.path.expanduser(args.output_basedir), output_dir)
     os.makedirs(output_fullpath, exist_ok=True)
-    
+
     #logger.configure(output_fullpath)
 
     return output_fullpath
 
 def init_logger(args):
+    """init_logger"""
     tmp_path = create_output_dir(args)
     # set up logger
-    global logger
-    logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    global LOGGER
+    LOGGER = configure(tmp_path, ["stdout", "csv", "tensorboard"])
 
-    return logger
+    return LOGGER
 
 def com_print(text):
-    logger.log(text)
+    """com_print"""
+    LOGGER.log(text)
 
 def get_model_file_name(args):
+    """get_model_file_name"""
     return args.env + '-' + args.alg + '-' + args.nn + '-' + str(args.num_timesteps)
