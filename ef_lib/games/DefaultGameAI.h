2409e89a6df17f50622091c1fd7959420d2b39c5
MatPoliquin <mathieu.poliquin@gmail.com>
2025-01-24 16:43:00 +0800

    Fix clang-format warnings

--- ef_lib/games/DefaultGameAI.h
+++ ef_lib/games/DefaultGameAI.h
@@ -4,16 +4,15 @@
 #include "../utils/data.h"
 #include "memory.h"
 
-class DefaultGameAI : public GameAILocal
-{
+class DefaultGameAI : public GameAILocal {
 public:
-    virtual void Init(void* ram_ptr, int ram_size);
-    virtual void Think(bool buttons[GAMEAI_MAX_BUTTONS], int player,
-                       const void* frame_data, unsigned int frame_width,
-                       unsigned int frame_height, unsigned int frame_pitch,
-                       unsigned int pixel_format);
+  virtual void Init(void *ram_ptr, int ram_size);
+  virtual void Think(bool buttons[GAMEAI_MAX_BUTTONS], int player,
+                     const void *frame_data, unsigned int frame_width,
+                     unsigned int frame_height, unsigned int frame_pitch,
+                     unsigned int pixel_format);
 
 private:
-    RetroModel* model;
-    RetroModelFrameData input;
+  RetroModel *model;
+  RetroModelFrameData input;
 };
