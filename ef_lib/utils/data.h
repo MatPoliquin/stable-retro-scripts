8caacd8bcec881e1e71e8e291faf776d6997be69
MatPoliquin <mathieu.poliquin@gmail.com>
2025-01-24 17:07:48 +0800

    clang.yml

--- ef_lib/utils/data.h
+++ ef_lib/utils/data.h
@@ -22,73 +22,74 @@ namespace Retro {
 
 class GameData {
 public:
-	bool load(const std::string& filename);
-	bool load(std::istream* stream);
+  bool load(const std::string &filename);
+  bool load(std::istream *stream);
 
-	bool save(const std::string& filename) const;
-	bool save(std::ostream* stream) const;
+  bool save(const std::string &filename) const;
+  bool save(std::ostream *stream) const;
 
-	void reset();
-	void restart();
+  void reset();
+  void restart();
 
-	static std::string dataPath(const std::string& hint = ".");
+  static std::string dataPath(const std::string &hint = ".");
 
-	AddressSpace& addressSpace() { return m_mem; }
-	const AddressSpace& addressSpace() const { return m_mem; }
-	void updateRam();
+  AddressSpace &addressSpace() { return m_mem; }
+  const AddressSpace &addressSpace() const { return m_mem; }
+  void updateRam();
 
-	void setTypes(const std::vector<DataType> types);
-	void setButtons(const std::vector<std::string>& names);
-	std::vector<std::string> buttons() const;
+  void setTypes(const std::vector<DataType> types);
+  void setButtons(const std::vector<std::string> &names);
+  std::vector<std::string> buttons() const;
 
-	void setActions(const std::vector<std::vector<std::vector<std::string>>>& actions);
-	std::map<int, std::set<int>> validActions() const;
-	unsigned filterAction(unsigned) const;
+  void
+  setActions(const std::vector<std::vector<std::vector<std::string>>> &actions);
+  std::map<int, std::set<int>> validActions() const;
+  unsigned filterAction(unsigned) const;
 
-	Datum lookupValue(const std::string& name);
-	Variant lookupValue(const std::string& name) const;
-	//Datum lookupValue(const TypedSearchResult&);
-	//int64_t lookupValue(const TypedSearchResult&) const;
-	std::unordered_map<std::string, Datum> lookupAll();
-	std::unordered_map<std::string, int64_t> lookupAll() const;
+  Datum lookupValue(const std::string &name);
+  Variant lookupValue(const std::string &name) const;
+  // Datum lookupValue(const TypedSearchResult&);
+  // int64_t lookupValue(const TypedSearchResult&) const;
+  std::unordered_map<std::string, Datum> lookupAll();
+  std::unordered_map<std::string, int64_t> lookupAll() const;
 
-	void setValue(const std::string& name, int64_t);
-	void setValue(const std::string& name, const Variant&);
+  void setValue(const std::string &name, int64_t);
+  void setValue(const std::string &name, const Variant &);
 
-	int64_t lookupDelta(const std::string& name) const;
+  int64_t lookupDelta(const std::string &name) const;
 
-	Variable getVariable(const std::string& name) const;
-	void setVariable(const std::string& name, const Variable&);
-	void removeVariable(const std::string& name);
+  Variable getVariable(const std::string &name) const;
+  void setVariable(const std::string &name, const Variable &);
+  void removeVariable(const std::string &name);
 
-	std::unordered_map<std::string, Variable> listVariables() const;
-	size_t numVariables() const;
+  std::unordered_map<std::string, Variable> listVariables() const;
+  size_t numVariables() const;
 
-	void search(const std::string& name, int64_t value);
-	void deltaSearch(const std::string& name, Operation op, int64_t reference);
-	size_t numSearches() const;
+  void search(const std::string &name, int64_t value);
+  void deltaSearch(const std::string &name, Operation op, int64_t reference);
+  size_t numSearches() const;
 
-	std::vector<std::string> listSearches() const;
-	//Search* getSearch(const std::string& name);
-	void removeSearch(const std::string& name);
+  std::vector<std::string> listSearches() const;
+  // Search* getSearch(const std::string& name);
+  void removeSearch(const std::string &name);
 
 #ifdef USE_CAPNP
-	bool loadSearches(const std::string& filename);
-	bool saveSearches(const std::string& filename) const;
+  bool loadSearches(const std::string &filename);
+  bool saveSearches(const std::string &filename) const;
 #endif
 
 private:
-	AddressSpace m_mem;
-	AddressSpace m_cloneMem;
-	AddressSpace m_lastMem;
-	std::vector<DataType> m_types;
+  AddressSpace m_mem;
+  AddressSpace m_cloneMem;
+  AddressSpace m_lastMem;
+  std::vector<DataType> m_types;
 
-	std::map<int, std::set<int>> m_actions;
-	std::vector<std::string> m_buttons;
+  std::map<int, std::set<int>> m_actions;
+  std::vector<std::string> m_buttons;
 
-	std::unordered_map<std::string, Variable> m_vars;
-	//std::unordered_map<std::string, Search> m_searches;
-	std::unordered_map<std::string, AddressSpace> m_searchOldMem;
-	std::unordered_map<std::string, std::unique_ptr<Variant>> m_customVars;
+  std::unordered_map<std::string, Variable> m_vars;
+  // std::unordered_map<std::string, Search> m_searches;
+  std::unordered_map<std::string, AddressSpace> m_searchOldMem;
+  std::unordered_map<std::string, std::unique_ptr<Variant>> m_customVars;
 };
-}
+} // namespace Retro
--- ef_lib/utils/unistd.h
+++ ef_lib/utils/unistd.h
@@ -1,5 +1,5 @@
 #ifndef _UNISTD_H
-#define _UNISTD_H    1
+#define _UNISTD_H 1
 
 /* This file intended to serve as a drop-in replacement for
  *  unistd.h on Windows.
@@ -7,22 +7,24 @@
  *  Original file from: http://stackoverflow.com/a/826027
  */
 
-#include <stdlib.h>
 #include <io.h>
-//#include <getopt.h> /* getopt at: https://gist.github.com/bikerm16/1b75e2dd20d839dcea58 */
+#include <stdlib.h>
+//#include <getopt.h> /* getopt at:
+// https://gist.github.com/bikerm16/1b75e2dd20d839dcea58 */
+#include <direct.h>  /* for _getcwd() and _chdir() */
 #include <process.h> /* for getpid() and the exec..() family */
-#include <direct.h> /* for _getcwd() and _chdir() */
 
 #define srandom srand
 #define random rand
 
 /* Values for the second argument to access.
    These may be OR'd together.  */
-#define R_OK    4       /* Test for read permission.  */
-#define W_OK    2       /* Test for write permission.  */
-#define X_OK    R_OK    /* execute permission - unsupported in Windows,
-                           use R_OK instead. */
-#define F_OK    0       /* Test for existence.  */
+#define R_OK 4 /* Test for read permission.  */
+#define W_OK 2 /* Test for write permission.  */
+#define X_OK                                                                   \
+  R_OK         /* execute permission - unsupported in Windows,                 \
+                  use R_OK instead. */
+#define F_OK 0 /* Test for existence.  */
 
 #define access _access
 #define dup2 _dup2
@@ -47,13 +49,13 @@
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
 /* should be in some equivalent to <sys/types.h> */
-//typedef __int8            int8_t;
-typedef __int16           int16_t; 
-typedef __int32           int32_t;
-typedef __int64           int64_t;
-typedef unsigned __int8   uint8_t;
-typedef unsigned __int16  uint16_t;
-typedef unsigned __int32  uint32_t;
-typedef unsigned __int64  uint64_t;
+// typedef __int8            int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
 
 #endif /* unistd.h  */
