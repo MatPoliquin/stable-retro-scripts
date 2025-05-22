1ae4b09406f377f2d8f0df8e786ebed5e8836666
MatPoliquin <mathieu.poliquin@gmail.com>
2025-05-22 19:36:15 +0800

    Remove duplicate nhl94 obs wrapper code

--- game_wrappers/nhl94_obs.py
+++ game_wrappers/nhl94_obs.py
@@ -14,14 +14,26 @@ from game_wrappers.nhl94_rf import register_functions
 from game_wrappers.nhl94_ai import NHL94AISystem
 from game_wrappers.nhl94_gamestate import NHL94GameState
 
-NUM_PARAMS = 24
+NUM_PARAMS_1ON1 = 16
+NUM_PARAMS_2ON2 = 24
 
 class NHL94Observation2PEnv(gym.Wrapper):
     def __init__(self, env, args, num_players, rf_name):
         gym.Wrapper.__init__(self, env)
 
-        low = np.array([-1] * NUM_PARAMS, dtype=np.float32)
-        high = np.array([1] * NUM_PARAMS, dtype=np.float32)
+        self.NUM_PARAMS = 0
+        self.num_players_per_team = 0
+        if args.env == 'NHL941on1-Genesis':
+            self.num_players_per_team = 1
+            self.NUM_PARAMS = NUM_PARAMS_1ON1
+        else:
+            self.num_players_per_team = 2
+            self.NUM_PARAMS = NUM_PARAMS_2ON2
+
+        self.game_state = NHL94GameState(self.num_players_per_team)
+
+        low = np.array([-1] * self.NUM_PARAMS, dtype=np.float32)
+        high = np.array([1] * self.NUM_PARAMS, dtype=np.float32)
         self.observation_space = spaces.Box(low, high, dtype=np.float32)
 
         self.reward_function = None
@@ -36,8 +48,6 @@ class NHL94Observation2PEnv(gym.Wrapper):
         self.num_players = num_players
         if num_players == 2:
             self.action_space = gym.spaces.MultiBinary(self.num_buttons)
-
-        self.game_state = NHL94GameState(2)
 
         self.ai_sys = NHL94AISystem(args, env, None)
 
@@ -55,9 +65,9 @@ class NHL94Observation2PEnv(gym.Wrapper):
     def reset(self, **kwargs):
         state, info = self.env.reset(**kwargs)
 
-        self.state = tuple([0] * NUM_PARAMS)
+        self.state = tuple([0] * self.NUM_PARAMS)
 
-        self.game_state = NHL94GameState(2)
+        self.game_state = NHL94GameState(self.num_players_per_team)
         self.ram_inited = False
         self.b_button_pressed = False
         self.c_button_pressed = False
@@ -114,34 +124,43 @@ class NHL94Observation2PEnv(gym.Wrapper):
 
         self.game_state.EndFrame()
 
+        if self.num_players_per_team == 1:
+            self.state = (self.game_state.normalized_p1_x, self.game_state.normalized_p1_y, \
+                        self.game_state.normalized_p1_velx, self.game_state.normalized_p1_vely, \
+                        self.game_state.normalized_p2_x, self.game_state.normalized_p2_y, \
+                        self.game_state.normalized_p2_velx, self.game_state.normalized_p2_vely, \
+                        self.game_state.normalized_puck_x, self.game_state.normalized_puck_y, \
+                        self.game_state.normalized_puck_velx, self.game_state.normalized_puck_vely, \
+                        self.game_state.normalized_g2_x, self.game_state.normalized_g2_y, \
+                        self.game_state.normalized_player_haspuck, self.game_state.normalized_goalie_haspuck)
+
+        elif self.num_players_per_team == 2:
+            p1_x, p1_y = self.game_state.normalized_p1_x, self.game_state.normalized_p1_y
+            p1_vel_x, p1_vel_y = self.game_state.normalized_p1_velx, self.game_state.normalized_p1_vely
+            p1_2_x, p1_2_y = self.game_state.normalized_p1_2_x, self.game_state.normalized_p1_2_y
+            p1_2_vel_x, p1_2_vel_y = self.game_state.normalized_p1_2_velx, self.game_state.normalized_p1_2_vely
+
+            # First two slots is for pos/vel of player beeing controled (empty or full star)
+            # So swap them if necessary
+            if self.game_state.p1_control == 2:
+                p1_x, p1_2_x = p1_2_x, p1_x
+                p1_y, p1_2_y = p1_2_y, p1_y
+                p1_vel_x, p1_2_vel_x = p1_2_vel_x, p1_vel_x
+                p1_vel_y, p1_2_vel_y = p1_2_vel_y, p1_vel_y
 
 
-        p1_x, p1_y = self.game_state.normalized_p1_x, self.game_state.normalized_p1_y
-        p1_vel_x, p1_vel_y = self.game_state.normalized_p1_velx, self.game_state.normalized_p1_vely
-        p1_2_x, p1_2_y = self.game_state.normalized_p1_2_x, self.game_state.normalized_p1_2_y
-        p1_2_vel_x, p1_2_vel_y = self.game_state.normalized_p1_2_velx, self.game_state.normalized_p1_2_vely
-
-        # First two slots is for pos/vel of player beeing controled (empty or full star)
-        # So swap them if necessary
-        if self.game_state.p1_control == 2:
-            p1_x, p1_2_x = p1_2_x, p1_x
-            p1_y, p1_2_y = p1_2_y, p1_y
-            p1_vel_x, p1_2_vel_x = p1_2_vel_x, p1_vel_x
-            p1_vel_y, p1_2_vel_y = p1_2_vel_y, p1_vel_y
-
-
-        self.state = (p1_x, p1_y, \
-                     p1_vel_x, p1_vel_y, \
-                     p1_2_x, p1_2_y, \
-                     p1_2_vel_x, p1_2_vel_y, \
-                     self.game_state.normalized_p2_x, self.game_state.normalized_p2_y, \
-                     self.game_state.normalized_p2_velx, self.game_state.normalized_p2_vely, \
-                     self.game_state.normalized_p2_2_x, self.game_state.normalized_p2_2_y, \
-                     self.game_state.normalized_p2_2_velx, self.game_state.normalized_p2_2_vely, \
-                     self.game_state.normalized_puck_x, self.game_state.normalized_puck_y, \
-                     self.game_state.normalized_puck_velx, self.game_state.normalized_puck_vely, \
-                     self.game_state.normalized_g2_x, self.game_state.normalized_g2_y, \
-                     self.game_state.normalized_player_haspuck, self.game_state.normalized_goalie_haspuck)
+            self.state = (p1_x, p1_y, \
+                        p1_vel_x, p1_vel_y, \
+                        p1_2_x, p1_2_y, \
+                        p1_2_vel_x, p1_2_vel_y, \
+                        self.game_state.normalized_p2_x, self.game_state.normalized_p2_y, \
+                        self.game_state.normalized_p2_velx, self.game_state.normalized_p2_vely, \
+                        self.game_state.normalized_p2_2_x, self.game_state.normalized_p2_2_y, \
+                        self.game_state.normalized_p2_2_velx, self.game_state.normalized_p2_2_vely, \
+                        self.game_state.normalized_puck_x, self.game_state.normalized_puck_y, \
+                        self.game_state.normalized_puck_velx, self.game_state.normalized_puck_vely, \
+                        self.game_state.normalized_g2_x, self.game_state.normalized_g2_y, \
+                        self.game_state.normalized_player_haspuck, self.game_state.normalized_goalie_haspuck)
 
         ob = self.state
 
